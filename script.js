d6489bdd27ef36adc96f9f71e0ba8b31f4724f94
aksenov-web <aksenov@inbox.lv>
2020-08-19 16:01:50 +0300

    Plus button function added

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let main_field = document.getElementById('main-field');
-let n = 43;
+let n = 42;
+let plus_button = document.getElementById('plus_button');
+plus_button.addEventListener('click', function(){
+  n = n + 1;
+  let buttons = document.createElement('button');
+  main_field.appendChild(buttons);
+  buttons.textContent = n;
+  console.log(n);
+  if (n % 3) {
+    buttons.style.background = "";
+  }
+  else {
+    buttons.style.background = "green";
+    buttons.addEventListener('click', function(){
+      let green_button_encrease = Number(buttons.textContent) + 1;
+      buttons.textContent = green_button_encrease;
+    })}
 
+})
 
-for (let i = 1; i < n; i++) {
+for (let i = 1; i <= n; i++) {
   let buttons = document.createElement('button');
   main_field.appendChild(buttons);
   buttons.textContent = i;
@@ -12,10 +29,11 @@ for (let i = 1; i < n; i++) {
   else {
     buttons.style.background = "green";
     buttons.addEventListener('click', function(){
-      let green = Number(buttons.textContent) + 1;
-      buttons.textContent = green;
+      let green_button_encrease = Number(buttons.textContent) + 1;
+      buttons.textContent = green_button_encrease;
     })}
 }
+
 
 /*function func1 (button) {
   for (let i = 0; i < 42; i) {
