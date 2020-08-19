76b13b6ac028bdb9ab4832a32e6fc4ca9b7dd118
aksenov-web <aksenov@inbox.lv>
2020-08-19 15:10:03 +0300

    added numbers and onclick action

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let main_field = document.getElementById('main-field');
-let buttons = document.createElement('button');
 
-for (let i = 0; i < 42; i++) {
-  main_field.appendChild(
-    buttons.cloneNode()
-  );
+
+
+for (let i = 1; i < 43; i++) {
+  let buttons = document.createElement('button');
+  main_field.appendChild(buttons);
+  buttons.textContent = i;
+  if (i % 3) {
+    buttons.style.background = "";
+  }
+  else {
+    buttons.style.background = "green";
+    buttons.addEventListener('click', function(){
+      let green = i++;
+      buttons.textContent = green;
+    })}
 }
 
-function func1 (button) {
+/*function func1 (button) {
   for (let i = 0; i < 42; i) {
     i +=3;
     let nth = i;
     let cell = document.querySelector('button:nth-child(' + nth + ')');
     cell.style.background = "green";
+
     console.log(func1);
 }}
 func1();
+*/
