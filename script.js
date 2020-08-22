a7dacda71a5f6b79dc0ae7d1148ba6b2f2dcb9c7
aksenov-web <aksenov@inbox.lv>
2020-08-22 14:11:30 +0300

    Green function udated;

--- script.js
+++ script.js
@@ -4,32 +4,23 @@ let plus_button = document.getElementById('plus_button');
 let color = document.getElementsByClassName('colors');
 let colorOne = "yellow";
 let colorTwo = "blue";
+let n = 0;
+
 
 //Button plus
-let buttons = document.createElement('button');
 plus_button.addEventListener('click', function(){
   i = i + 1;
-  let buttons = document.createElement('button');
-  main_field.appendChild(buttons);
-  buttons.textContent = i;
-  if (i % 3 == 0) {
-    buttons.style.background = colorOne;
-    buttons.addEventListener('click', function(){
-      let green_button_encrease = Number(buttons.textContent) + 1;
-      buttons.textContent = green_button_encrease;
-      if (buttons.textContent % 2 == 0) {
-        buttons.style.background = colorTwo;
-      }
-      else {
-        buttons.style.background = colorOne;
-      }
-    })}
-  if (buttons.textContent % 2 == 0) {
-    buttons.style.background = colorTwo;
-  }})
+  green(i);
+})
 
 // 42 buttons
 for (let i = 1; i <= 42; i++) {
+  green(i);
+}
+console.log(color);
+
+
+function green(i) {
   let buttons = document.createElement('button');
   main_field.appendChild(buttons);
   buttons.textContent = i;
@@ -51,29 +42,9 @@ for (let i = 1; i <= 42; i++) {
     })}
   if (buttons.textContent % 2 == 0) {
     buttons.style.background = colorTwo;
-  }}
-console.log(color);
-
-
-
-
-
-
-/*function green(i) {
-  let buttons = document.createElement('button');
-  main_field.appendChild(buttons);
-  buttons.textContent = i;
-  if (i % 3) {
-    buttons.style.background = "";
   }
-  else {
-    buttons.style.background = "green";
-    buttons.addEventListener('click', function(){
-      let green_button_encrease = Number(buttons.textContent) + 1;
-      buttons.textContent = green_button_encrease;
-    })}
 }
-*/
+
 /*function func1 (button) {
   for (let i = 0; i < 42; i) {
     i +=3;
