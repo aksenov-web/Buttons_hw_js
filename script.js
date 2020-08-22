cb6ad1c93f1ecacb965ce28db97ead969116c3dd
aksenov-web <aksenov@inbox.lv>
2020-08-22 13:49:51 +0300

    Added:
    - color selector;
    - colorOne colorTwo var;

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let main_field = document.getElementById('main-field');
 let i = 42;
 let plus_button = document.getElementById('plus_button');
+let color = document.getElementsByClassName('colors');
+let colorOne = "yellow";
+let colorTwo = "blue";
+
+//Button plus
 let buttons = document.createElement('button');
 plus_button.addEventListener('click', function(){
   i = i + 1;
@@ -8,45 +13,51 @@ plus_button.addEventListener('click', function(){
   main_field.appendChild(buttons);
   buttons.textContent = i;
   if (i % 3 == 0) {
-    buttons.style.background = "green";
+    buttons.style.background = colorOne;
     buttons.addEventListener('click', function(){
       let green_button_encrease = Number(buttons.textContent) + 1;
       buttons.textContent = green_button_encrease;
       if (buttons.textContent % 2 == 0) {
-    buttons.style.background = "red";
-  }
+        buttons.style.background = colorTwo;
+      }
       else {
-        buttons.style.background = "green";
+        buttons.style.background = colorOne;
       }
     })}
   if (buttons.textContent % 2 == 0) {
-    buttons.style.background = "red";
-  }
-})
+    buttons.style.background = colorTwo;
+  }})
 
+// 42 buttons
 for (let i = 1; i <= 42; i++) {
   let buttons = document.createElement('button');
   main_field.appendChild(buttons);
   buttons.textContent = i;
+
   if (buttons.textContent % 2 == 0) {
-    buttons.style.background = "red";
+    buttons.style.background = colorTwo;
   }
   if (i % 3 == 0) {
-    buttons.style.background = "green";
+    buttons.style.background = colorOne;
     buttons.addEventListener('click', function(){
       let green_button_encrease = Number(buttons.textContent) + 1;
       buttons.textContent = green_button_encrease;
       if (buttons.textContent % 2 == 0) {
-    buttons.style.background = "red";
-  }
+    buttons.style.background = colorTwo;
+      }
       else {
-        buttons.style.background = "green";
+        buttons.style.background = colorOne;
       }
     })}
   if (buttons.textContent % 2 == 0) {
-    buttons.style.background = "red";
-  }
-}
+    buttons.style.background = colorTwo;
+  }}
+console.log(color);
+
+
+
+
+
 
 /*function green(i) {
   let buttons = document.createElement('button');
